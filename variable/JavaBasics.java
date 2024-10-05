97fed7d8225be4ea0d26331c63ae5f957a029576
shshalam68 <a.k.mshahalam68@gmail.com>
2024-10-05 23:26:48 +0600

    first commit

---
+++ variable/JavaBasics.java
@@ -0,0 +1,10 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        int a= 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Soykot Alam";
+        System.out.println(name);
+    }
+}
